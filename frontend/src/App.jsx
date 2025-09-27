2aab4a68bcdaea931040008c1277a20edb96832f
am-ndaa <148898058+am-ndaa@users.noreply.github.com>
2025-09-27 16:45:17 -0400

    implemented routes to dashboard after user auth

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import AuthPage from './pages/auth/authPage'
-import './App.css'
+// import { useState } from 'react'
+import AuthPage from './pages/auth/authPage';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import './App.css';
+import {useAuth0} from "@auth0/auth0-react";
+import DashboardPage from './pages/dashboard/DashboardPage';
 
 function App() {
+  const { isAuthenticated } = useAuth0();
+
   return(
-    <AuthPage />
+    <Router>
+      <Routes>
+        <Route
+        path = "/"
+        element={isAuthenticated ? <DashboardPage /> : <AuthPage />}
+        />
+        <Route
+        path = "/dashboard"
+        element = {isAuthenticated ? <DashboardPage /> : <AuthPage />}
+        />
+      </Routes>
+    </Router>
   );
 }
 
